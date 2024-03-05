40cd22eb7ff11e67b68a3a3186d0994f6ff3cda6
Leaundrae Mckinney <pytechdrae@gmail.com>
2024-03-05 01:32:56 -0500

    Implement logic and resultset

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { styles } from "./styles";
 import { ActionButton, ChoiceGroup, IChoiceGroupOption } from "@fluentui/react";
+import Web from "./Web";
 
 interface iQuestion {
   id: number;
@@ -8,10 +9,18 @@ interface iQuestion {
   response: number;
 }
 
+interface iQuestionResponse {
+  questionNumber: number;
+  responseNumber: number;
+}
+
 const App: React.FC = () => {
   const [questions, setQuestions] = React.useState<iQuestion[]>([]);
   const [questionNumber, setQuestionNumber] = React.useState<number>(1);
-  const [response, setResponse] = React.useState<iQuestion[]>([]);
+  const [currentResponseNumber, setCurrentResponseNumber] = React.useState(-1);
+  const [responses, setResponses] = React.useState<iQuestionResponse[]>([]);
+  const [selectedOption, setSelectedOption] = React.useState<string | null>("");
+  const [showGraph, setShowGraph] = React.useState(false);
 
   React.useEffect(() => {
     fetch("questions.json")
@@ -23,26 +32,25 @@ const App: React.FC = () => {
         console.error("Error fetching JSON: ", error);
       });
   }, []);
-  const Questions = React.useMemo(() => {
-    return questions.map((q: iQuestion, idx: number) => {
-      return <div key={idx}>{q.query}</div>;
-    });
-  }, [questions]);
 
-  const responseOptions: IChoiceGroupOption[] = [
-    { key: "1", text: "1" },
-    { key: "2", text: "2" },
-    { key: "3", text: "3" },
-    { key: "4", text: "4" },
-    { key: "5", text: "5" },
-  ];
+  const responseOptions: IChoiceGroupOption[] = React.useMemo(
+    () => [
+      { key: "1", text: "1" },
+      { key: "2", text: "2" },
+      { key: "3", text: "3" },
+      { key: "4", text: "4" },
+      { key: "5", text: "5" },
+    ],
+    []
+  );
 
   const CurrentQuestion = React.useMemo(() => {
     return questions.find((q, idx) => q.id === questionNumber);
   }, [questions, questionNumber]);
+
   const buttonText = React.useMemo(() => {
     if (!CurrentQuestion) return "Error";
-    if (CurrentQuestion.id === questions.length - 1) return "Submit";
+    if (CurrentQuestion.id === questions.length) return "Submit";
     return "Next";
   }, [CurrentQuestion, questions]);
 
@@ -50,24 +58,69 @@ const App: React.FC = () => {
     ev?: React.FormEvent<HTMLElement | HTMLInputElement>,
     option?: IChoiceGroupOption | undefined
   ) => {
-    console.log(option);
+    const key = option?.key;
+    if (!key) return;
+    setSelectedOption(key);
+    setCurrentResponseNumber(parseInt(key));
   };
 
-  const onActionButtonClicked = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    if (questionNumber < questions.length - 1) {
-      setQuestionNumber(questionNumber + 1);
-    }
-  };
+  const onActionButtonClicked = React.useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+      if (questionNumber < questions.length) {
+        setQuestionNumber(questionNumber + 1);
+        if (currentResponseNumber > 0) {
+          const response: iQuestionResponse = {
+            questionNumber: questionNumber,
+            responseNumber: currentResponseNumber,
+          };
+          console.log(response);
+          setResponses((prev) => [...prev, response]);
+        }
+      } else {
+        setShowGraph(true);
+      }
+      setSelectedOption(null);
+      setCurrentResponseNumber(-1);
+    },
+    [questionNumber, questions, currentResponseNumber]
+  );
+
+  const RenderedCard = React.useMemo(() => {
+    return (
+      CurrentQuestion && (
+        <div className={styles.cardContainer}>
+          <div>{`${questionNumber}. ${CurrentQuestion.query}`}</div>
+          <ChoiceGroup
+            options={responseOptions}
+            onChange={onOptionSelected}
+            selectedKey={selectedOption}
+          />
+          <ActionButton
+            text={buttonText}
+            onClick={onActionButtonClicked}
+            disabled={selectedOption === null}
+          />
+        </div>
+      )
+    );
+  }, [
+    CurrentQuestion,
+    selectedOption,
+    onActionButtonClicked,
+    buttonText,
+    responseOptions,
+    questionNumber,
+  ]);
 
   return (
     <div className={styles.appContainer}>
-      {CurrentQuestion && (
-        <div className={styles.cardContainer}>
-          <div>{CurrentQuestion.query}</div>
-          <ChoiceGroup options={responseOptions} onChange={onOptionSelected} />
-          <ActionButton text={buttonText} onClick={onActionButtonClicked} />
+      {showGraph ? (
+        <div className={styles.graphContainer}>
+          <Web selections={responses} />
         </div>
+      ) : (
+        RenderedCard
       )}
     </div>
   );
